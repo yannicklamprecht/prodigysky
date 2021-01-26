3c69b9dc57905f5832c0855acc88eaf3eea5941c
Yannick Lamprecht <yannicklamprecht@live.de>
2021-01-26 13:49:48 +0100

    split into multimodules

---
+++ Core/src/main/java/fr/cocoraid/prodigysky/nms/Impl1_16_R3.java
@@ -0,0 +1,17 @@
+package fr.cocoraid.prodigysky.nms;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Impl1_16_R3 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
